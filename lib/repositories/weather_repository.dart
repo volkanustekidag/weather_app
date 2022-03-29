49f1bb5ead5dbc409d599fa616201ee526650654
volkanustekidag <volkanustekidg@gmail.com>
2022-03-29 16:32:54 +0300

    weather repository created

---
+++ lib/repositories/weather_repository.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:weather_app/models/weather.dart';
+
+const baseUrl = 'https://www.metaweather.com/';
+final locationUrl = (city) => "${baseUrl}/api/location/search/?query=${city}";
+final weatherUrl = (locationId) => "${baseUrl}/api/location/${locationId}";
+
+class WeatherRepository {
+  final http.Client httpClient;
+
+  WeatherRepository({@required this.httpClient});
+
+  Future<List> getLocationIdFromCity(String city) async {
+    final response = await this.httpClient.get(Uri.parse(locationUrl(city)));
+    if (response.statusCode == 200) {
+      final cities = jsonDecode(response.body) as List;
+      return cities;
+    } else {
+      throw Exception('Error getting location id of :' + city);
+    }
+  }
+
+  Future<Weather> fetchWeather(int location) async {
+    final response = await this.httpClient.get(Uri.parse(weatherUrl(location)));
+    if (response.statusCode == 200) {
+      final weatherJson = jsonDecode(response.body);
+      return Weather.fromJson(weatherJson);
+    } else {
+      throw Exception('Error getting location id of :' + location.toString());
+    }
+  }
+
+  Future<Weather> getWeatherFromCity(String city) async {
+    final List locationId = await getLocationIdFromCity(city);
+    return fetchWeather(locationId.first);
+  }
+}
