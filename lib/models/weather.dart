1f84f5c1f63a5a063f3d5addfae81bc498cf9395
volkanustekidag <volkanustekidg@gmail.com>
2022-03-29 14:47:38 +0300

    weather model created

--- lib/models/weather.dart
+++ lib/models/weather.dart
@@ -1,23 +1,105 @@
 import 'package:equatable/equatable.dart';
 
 class Weather extends Equatable {
-  final int id;
-  final String weatherStateName;
-  final String weatherStateAbbr;
-  final String windDirectionCompass;
-  final String created;
-  final String applicableDate;
-  final double minTemp;
-  final double maxTemp;
-  final double theTemp;
-  final double windSpeed;
-  final double windDirection;
-  final int airPressure;
-  final int humidity;
-  final double visibility;
-  final int predictability;
+  List<ConsolidatedWeather> consolidatedWeather;
+  String time;
+  String sunRise;
+  String sunSet;
+  String timezoneName;
+  Parent parent;
+  String title;
+  String locationType;
+  int woeid;
+  String lattLong;
+  String timezone;
 
-  const Weather(
+  Weather(
+      {this.consolidatedWeather,
+      this.time,
+      this.sunRise,
+      this.sunSet,
+      this.timezoneName,
+      this.parent,
+      this.title,
+      this.locationType,
+      this.woeid,
+      this.lattLong,
+      this.timezone});
+
+  Weather.fromJson(Map<String, dynamic> json) {
+    if (json['consolidated_weather'] = null) {
+      consolidatedWeather = <ConsolidatedWeather>[];
+      json['consolidated_weather'].forEach((v) {
+        consolidatedWeather.add(new ConsolidatedWeather.fromJson(v));
+      });
+    }
+    time = json['time'];
+    sunRise = json['sun_rise'];
+    sunSet = json['sun_set'];
+    timezoneName = json['timezone_name'];
+    parent = json['parent'] = null ? new Parent.fromJson(json['parent']) : null;
+    title = json['title'];
+    locationType = json['location_type'];
+    woeid = json['woeid'];
+    lattLong = json['latt_long'];
+    timezone = json['timezone'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.consolidatedWeather = null) {
+      data['consolidated_weather'] =
+          this.consolidatedWeather.map((v) => v.toJson()).toList();
+    }
+    data['time'] = this.time;
+    data['sun_rise'] = this.sunRise;
+    data['sun_set'] = this.sunSet;
+    data['timezone_name'] = this.timezoneName;
+    if (this.parent = null) {
+      data['parent'] = this.parent.toJson();
+    }
+    data['title'] = this.title;
+    data['location_type'] = this.locationType;
+    data['woeid'] = this.woeid;
+    data['latt_long'] = this.lattLong;
+    data['timezone'] = this.timezone;
+    return data;
+  }
+
+  @override
+  List<Object> get props => [
+        consolidatedWeather,
+        time,
+        sunRise,
+        sunSet,
+        timezoneName,
+        parent,
+        title,
+        locationType,
+        woeid,
+        lattLong,
+        timezone,
+      ];
+}
+
+class ConsolidatedWeather {
+  int id;
+  String weatherStateName;
+  String weatherStateAbbr;
+  String windDirectionCompass;
+  String created;
+  String applicableDate;
+  double minTemp;
+  double maxTemp;
+  double theTemp;
+  double windSpeed;
+  double windDirection;
+  double airPressure;
+  int humidity;
+  double visibility;
+  int predictability;
+
+  ConsolidatedWeather(
       {this.id,
       this.weatherStateName,
       this.weatherStateAbbr,
@@ -34,7 +116,66 @@ class Weather extends Equatable {
       this.visibility,
       this.predictability});
 
-  @override
-  // TODO: implement props
-  List<Object> get props => throw UnimplementedError();
+  ConsolidatedWeather.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    weatherStateName = json['weather_state_name'];
+    weatherStateAbbr = json['weather_state_abbr'];
+    windDirectionCompass = json['wind_direction_compass'];
+    created = json['created'];
+    applicableDate = json['applicable_date'];
+    minTemp = json['min_temp'];
+    maxTemp = json['max_temp'];
+    theTemp = json['the_temp'];
+    windSpeed = json['wind_speed'];
+    windDirection = json['wind_direction'];
+    airPressure = json['air_pressure'];
+    humidity = json['humidity'];
+    visibility = json['visibility'];
+    predictability = json['predictability'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['weather_state_name'] = this.weatherStateName;
+    data['weather_state_abbr'] = this.weatherStateAbbr;
+    data['wind_direction_compass'] = this.windDirectionCompass;
+    data['created'] = this.created;
+    data['applicable_date'] = this.applicableDate;
+    data['min_temp'] = this.minTemp;
+    data['max_temp'] = this.maxTemp;
+    data['the_temp'] = this.theTemp;
+    data['wind_speed'] = this.windSpeed;
+    data['wind_direction'] = this.windDirection;
+    data['air_pressure'] = this.airPressure;
+    data['humidity'] = this.humidity;
+    data['visibility'] = this.visibility;
+    data['predictability'] = this.predictability;
+    return data;
+  }
+}
+
+class Parent {
+  String title;
+  String locationType;
+  int woeid;
+  String lattLong;
+
+  Parent({this.title, this.locationType, this.woeid, this.lattLong});
+
+  Parent.fromJson(Map<String, dynamic> json) {
+    title = json['title'];
+    locationType = json['location_type'];
+    woeid = json['woeid'];
+    lattLong = json['latt_long'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['title'] = this.title;
+    data['location_type'] = this.locationType;
+    data['woeid'] = this.woeid;
+    data['latt_long'] = this.lattLong;
+    return data;
+  }
 }
