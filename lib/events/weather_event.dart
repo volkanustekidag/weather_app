44901b041827b451a8d591c9426a1327017c7045
volkanustekidag <volkanustekidg@gmail.com>
2022-03-30 14:26:49 +0300

    weather event created

---
+++ lib/events/weather_event.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+abstract class WeatherEvent extends Equatable {
+  const WeatherEvent();
+}
+
+class WeatherEventRequested extends WeatherEvent {
+  final String city;
+  const WeatherEventRequested({@required this.city});
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [city];
+}
+
+class WeatherEventRefresh extends WeatherEvent {
+  final String city;
+  const WeatherEventRefresh({@required this.city});
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [city];
+}
---
+++ lib/states/weather_state.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+import 'package:weather_app/models/weather.dart';
+
+abstract class WeatherState extends Equatable {
+  const WeatherState();
+  List<Object> get props => [];
+}
+
+class WeatherStateInitial extends WeatherState {
+  @override
+  // TODO: implement props
+  List<Object> get props => [];
+}
+
+class WeatherStateLoading extends WeatherState {}
+
+class WeatherStateSucces extends WeatherState {
+  final Weather weather;
+  const WeatherStateSucces({this.weather});
+
+  List<Object> get probs => [weather];
+}
+
+class WeatherStateFailure extends WeatherState {}
