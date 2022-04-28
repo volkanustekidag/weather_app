bdf947519e7ee407704ba9f7966f18e58a27d2e7
volkanustekidag <volkanustekidg@gmail.com>
2022-04-28 11:43:37 +0300

    Created selected weather bloc files

---
+++ lib/blocs/selected_weather_bloc/selectedweatherbloc_event.dart
@@ -0,0 +1,8 @@
+part of 'selectedweatherbloc_bloc.dart';
+
+abstract class SelectedweatherblocEvent extends Equatable {
+  const SelectedweatherblocEvent();
+
+  @override
+  List<Object> get props => [];
+}
---
+++ lib/blocs/selected_weather_bloc/selectedweatherbloc_state.dart
@@ -0,0 +1,10 @@
+part of 'selectedweatherbloc_bloc.dart';
+
+abstract class SelectedweatherblocState extends Equatable {
+  const SelectedweatherblocState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+class SelectedweatherblocInitial extends SelectedweatherblocState {}
