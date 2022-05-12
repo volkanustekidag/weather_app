55e9789a035b4af1917715bbce36f75d8a4ae1eb
volkanustekidag <volkanustekidg@gmail.com>
2022-05-12 08:50:05 +0300

    selected weather   container added

--- lib/screens/weather_screen/succes_screen/selected_weather.dart
+++ lib/screens/weather_screen/succes_screen/selected_weather.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:weather_app/models/weather.dart';
 import 'package:weather_app/screens/weather_screen/widgets/weather_condition_svg.dart';
-import 'package:weather_icons/weather_icons.dart';
 
 class SelectedWeather extends StatelessWidget {
   final weather, title;
@@ -12,33 +10,44 @@ class SelectedWeather extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       flex: 2,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            title,
-            style: TextStyle(color: Colors.white, fontSize: 35),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 80.0),
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.95,
+          decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.2),
+              borderRadius: BorderRadius.circular(10)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                title,
+                style: TextStyle(color: Colors.white, fontSize: 35),
+              ),
+              Text(
+                DateFormat('EEEE')
+                    .format(DateTime.parse(weather.applicableDate)),
+                style: TextStyle(color: Colors.white),
+              ),
+              mapWeatherConditionToIcon(
+                  weatherCondition: weather.weatherCondition),
+              Text(
+                weather.weatherStateName,
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              /* Text("Humidity: " + weather.humidity.toString() + "%",
+                  style: TextStyle(color: Colors.white, fontSize: 15)),
+              Text("Pressure: " + weather.airPressure.toStringAsFixed(0) + "hPa",
+                  style: TextStyle(color: Colors.white, fontSize: 15)),
+              Text("Wind: " + weather.windSpeed.toStringAsFixed(0) + "km",
+                  style: TextStyle(color: Colors.white, fontSize: 15)),*/
+              Text(
+                weather.maxTemp.toStringAsFixed(0) + "°",
+                style: TextStyle(color: Colors.white, fontSize: 50),
+              )
+            ],
           ),
-          Text(
-            DateFormat('EEEE').format(DateTime.parse(weather.applicableDate)),
-            style: TextStyle(color: Colors.white),
-          ),
-          mapWeatherConditionToIcon(weatherCondition: weather.weatherCondition),
-          Text(
-            weather.weatherStateName,
-            style: TextStyle(color: Colors.white, fontSize: 20),
-          ),
-          /* Text("Humidity: " + weather.humidity.toString() + "%",
-              style: TextStyle(color: Colors.white, fontSize: 15)),
-          Text("Pressure: " + weather.airPressure.toStringAsFixed(0) + "hPa",
-              style: TextStyle(color: Colors.white, fontSize: 15)),
-          Text("Wind: " + weather.windSpeed.toStringAsFixed(0) + "km",
-              style: TextStyle(color: Colors.white, fontSize: 15)),*/
-          Text(
-            weather.maxTemp.toStringAsFixed(0) + "°",
-            style: TextStyle(color: Colors.white, fontSize: 50),
-          )
-        ],
+        ),
       ),
     );
   }
